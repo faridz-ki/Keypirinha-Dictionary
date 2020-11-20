4dcdf0c5aec0de74c3f62f8a9f2ff2ccab47c0c6
Faridz Ibrahim <faridzkemal@gmail.com>
2020-11-20 22:00:39 +0000

    Added multiple language support

--- dictionary.py
+++ dictionary.py
@@ -1,24 +1,23 @@
 import keypirinha as kp 
 import keypirinha_util as kp_util
 import keypirinha_net as kp_net
-import json
+import json, re, urllib
 
 class dictionary(kp.Plugin):
 
-    API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+    API_URL = "https://api.dictionaryapi.dev/api/v2/entries/"
     IDLE_TIME = 0.25
     DICT_ITEMCAT = kp.ItemCategory.USER_BASE + 1
     ANSWER_ITEMCAT = kp.ItemCategory.USER_BASE + 2
     COPY_ITEMCAT = kp.ItemCategory.USER_BASE + 3
+    DEFAULT_LANG = 'en'
 
     def __init__(self):
         super().__init__()
 
-    def _build_url(self, query):
-        return self.API_URL + query
-
     def _parse_response(self, response):
         results = []
+        response = response.decode(encoding="utf-8", errors="strict")
         js = json.loads(response)
         for word in js:
             for definition in word["meanings"]:
@@ -27,13 +26,14 @@ class dictionary(kp.Plugin):
                     results.append((partOfSpeech, meaning["definition"]))
         return results
 
-    def _make_request(self, url):
+    def _make_request(self, query, lang):
+        url = self.API_URL + lang + '/' + urllib.parse.quote(query)
         opener = kp_net.build_urllib_opener()
         with opener.open(url) as conn:
             return conn.read()
 
-    def _dict_search(self, query):
-        responseList = self._parse_response(self._make_request(self._build_url(query)))
+    def _dict_search(self, query, lang):
+        responseList = self._parse_response(self._make_request(query, lang))
         return responseList
 
     def on_start(self):
@@ -62,14 +62,32 @@ class dictionary(kp.Plugin):
             return
         
         definitions = []
+        _user_input = ""
+        lang = self.DEFAULT_LANG
 
+        if ' ' in user_input.strip():
+            m = re.match((r"^:(?P<lang>[A-Za-z]{2})\s+(?P<terms>[^\s0-9]+)$"), user_input.strip())
+            if m is not None:
+                _user_input = m.group("terms")
+                lang = m.group("lang")
+        else:
+            _user_input = user_input.strip()
+
+        result = None
         try:
-            result = self._dict_search(user_input.lower())
+            result = self._dict_search(_user_input.lower(), lang)
+        except urllib.error.HTTPError as ex:
+            if len(_user_input) > 1:
+                definitions.append(self.create_error_item(
+                label=user_input,
+                short_desc="Word not found: " + _user_input,
+            ))
         except Exception as ex:
             definitions.append(self.create_error_item(
                 label=user_input,
                 short_desc="Error: " + str(ex),
             ))
+            
         
         if result:
             for x, y in result:
@@ -77,7 +95,7 @@ class dictionary(kp.Plugin):
                     return
                 definitions.append(self.create_item(               
                     category=self.ANSWER_ITEMCAT,
-                    label=str("(" + x + ") " + y),
+                    label="(" + x + ") " + y,
                     short_desc="Press Enter to copy the result",
                     target=y,
                     args_hint=kp.ItemArgsHint.FORBIDDEN,
